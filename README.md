a6b1907da4285646884c00876ee8835ba5d3d9e2
Joe Heffer <j.heffer@sheffield.ac.uk>
2025-07-24 10:51:00 +0100

    Add --num_records option

--- README.md
+++ README.md
@@ -10,6 +10,25 @@ pip install git+https://github.com/rcgsheffield/topdesk-synthetic-data.git
 
 # Usage
 
+Once installed, run `topdesk-synthetic-data` in the command line.
+
 ```bash
-topdesk-synthetic-data
+topdesk-synthetic-data --help
+```
+```
+usage: topdesk-synthetic-data [-h] [--log_level LOG_LEVEL] [--num_records NUM_RECORDS]
+
+TOPdesk synthetic data generator
+
+options:
+  -h, --help            show this help message and exit
+  --log_level LOG_LEVEL
+  --num_records, -n NUM_RECORDS
+
+```
+
+## Example
+
+```bash
+topdesk-synthetic-data -n 100
 ```
--- src/topdesk_synthetic_data/__main__.py
+++ src/topdesk_synthetic_data/__main__.py
@@ -21,6 +21,7 @@ def get_args() -> argparse.Namespace:
     """
     parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument("--log_level", default="INFO")
+    parser.add_argument("--num_records", "-n", type=int, default=100)
     return parser.parse_args()
 
 
@@ -33,21 +34,24 @@ def main():
     logger.info("Generating TOPdesk dummy data...")
 
     # Generate incident data
-    incidents = pandas.DataFrame.from_records(Incident.generate())
+    incidents = pandas.DataFrame.from_records(Incident.generate(num_records=args.num_records))
+    logger.info("Generated %d incident records", len(incidents.index))
     incidents.to_csv("topdesk_incidents_dummy.csv", index=False)
     logger.info("Wrote topdesk_incidents_dummy.csv")
     incidents.to_excel("topdesk_incidents_dummy.xlsx", index=False)
     logger.info("Wrote topdesk_incidents_dummy.xlsx")
 
     # Generate person data
-    people = pandas.DataFrame.from_records(Person.generate())
+    people = pandas.DataFrame.from_records(Person.generate(num_records=args.num_records))
+    logger.info("Generated %d person records", len(people.index))
     people.to_csv("topdesk_persons_dummy.csv", index=False)
     logger.info("Wrote topdesk_persons_dummy.csv")
     people.to_excel("topdesk_persons_dummy.xlsx", index=False)
     logger.info("Wrote topdesk_persons_dummy.xlsx")
 
     # Generate asset data
-    assets = pandas.DataFrame.from_records(Asset.generate())
+    assets = pandas.DataFrame.from_records(Asset.generate(num_records=args.num_records))
+    logger.info("Generated %d asset records", len(assets.index))
     assets.to_csv("topdesk_assets_dummy.csv", index=False)
     logger.info("Wrote topdesk_assets_dummy.csv")
     assets.to_excel("topdesk_assets_dummy.xlsx", index=False)

7b52e7813fff3b171a29de4f95b395c10dd6cb96
Joe Heffer <j.heffer@sheffield.ac.uk>
2025-07-24 10:35:23 +0100

    Add nice logging output

--- src/topdesk_synthetic_data/__main__.py
+++ src/topdesk_synthetic_data/__main__.py
@@ -19,7 +19,7 @@ def get_args() -> argparse.Namespace:
     https://docs.python.org/3/howto/argparse.html
     """
     parser = argparse.ArgumentParser(description=DESCRIPTION)
-    parser.add_argument("--log_level", default="WARNING")
+    parser.add_argument("--log_level", default="INFO")
     return parser.parse_args()
 
 
@@ -29,22 +29,28 @@ def main():
         format="%(name)s:%(asctime)s:%(levelname)s:%(message)s", level=args.log_level
     )
 
-    print("Generating TOPdesk dummy data...")
+    logger.info("Generating TOPdesk dummy data...")
 
     # Generate incident data
     incidents = pandas.DataFrame.from_records(Incident.generate())
     incidents.to_csv("topdesk_incidents_dummy.csv", index=False)
+    logger.info("Wrote topdesk_incidents_dummy.csv")
     incidents.to_excel("topdesk_incidents_dummy.xlsx", index=False)
+    logger.info("Wrote topdesk_incidents_dummy.xlsx")
 
     # Generate person data
     people = pandas.DataFrame.from_records(Person.generate())
     people.to_csv("topdesk_persons_dummy.csv", index=False)
+    logger.info("Wrote topdesk_persons_dummy.csv")
     people.to_excel("topdesk_persons_dummy.xlsx", index=False)
+    logger.info("Wrote topdesk_persons_dummy.xlsx")
 
     # Generate asset data
     assets = pandas.DataFrame.from_records(Asset.generate())
     assets.to_csv("topdesk_assets_dummy.csv", index=False)
+    logger.info("Wrote topdesk_assets_dummy.csv")
     assets.to_excel("topdesk_assets_dummy.xlsx", index=False)
+    logger.info("Wrote topdesk_assets_dummy.xlsx")
 
 
 if __name__ == "__main__":
